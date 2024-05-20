8e4d873bef6a03487281e1e5f834c08798ccf4ba
Namgel <namgeltamang14@gmail.com>
2024-05-20 15:47:55 +0545

    Configure redux toolkit

---
+++ src/app/apiSlice.ts
@@ -0,0 +1,22 @@
+import {
+	BaseQueryFn,
+	FetchArgs,
+	createApi,
+	fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "./constants";
+
+export interface Error {
+	data: { message: string; stack: string };
+	error: string;
+	status: number;
+}
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: BASE_URL,
+}) as unknown as BaseQueryFn<string | FetchArgs, unknown, Error, {}>;
+
+export const apiSlice = createApi({
+	baseQuery,
+	endpoints: () => ({}),
+});
