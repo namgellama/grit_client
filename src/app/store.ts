14bef002986c86b9b65a405351f7508335781c1d
Namgel <namgeltamang14@gmail.com>
2024-05-20 15:51:29 +0545

    Add additional requirements for redux configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./apiSlice";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(apiSlice.middleware),
+	devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,21 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./customer/pages/HomePage.tsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<Router>
-				<Routes>
-					<Route path="/" element={<App />}>
-						<Route path="/" element={<HomePage />} />
-					</Route>
-				</Routes>
-			</Router>
-		</ChakraProvider>
+		<Provider store={store}>
+			<ChakraProvider>
+				<Router>
+					<Routes>
+						<Route path="/" element={<App />}>
+							<Route path="/" element={<HomePage />} />
+						</Route>
+					</Routes>
+				</Router>
+			</ChakraProvider>
+		</Provider>
 	</React.StrictMode>
 );
