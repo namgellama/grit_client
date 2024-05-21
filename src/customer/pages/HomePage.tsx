662c7563251055d74ed66481af8f99131fe0b32d
Namgel <namgeltamang14@gmail.com>
2024-05-21 14:14:21 +0545

    Remove unnecessary code

--- src/customer/pages/HomePage.tsx
+++ src/customer/pages/HomePage.tsx
@@ -1,12 +1,7 @@
-import { useGetProductsQuery } from "../../app/product/productApiSlice";
 import CategoryList from "../components/CategoryList";
 import Hero from "../components/Hero";
 
 const HomePage = () => {
-	const { data: products } = useGetProductsQuery();
-
-	console.log(products);
-
 	return (
 		<>
 			<Hero />
