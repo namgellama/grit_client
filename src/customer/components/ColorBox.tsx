a5d1fee94a5f5382f6d42f43a67296303990a271
Namgel <namgeltamang14@gmail.com>
2024-05-22 17:18:06 +0545

    Change image on color box click

--- src/customer/components/ColorBox.tsx
+++ src/customer/components/ColorBox.tsx
@@ -1,10 +1,21 @@
 import { Box, Tooltip } from "@chakra-ui/react";
 import { Color } from "../../app/product/productApiSlice";
 
-const ColorBox = ({ color }: { color: Color }) => {
+interface Props {
+	color: Color;
+	setCurrentImage: (color: string) => void;
+}
+
+const ColorBox = ({ color, setCurrentImage }: Props) => {
 	return (
 		<Tooltip key={color.hexColor} label={color.colorName}>
-			<Box w={3} h={3} background={color.hexColor}></Box>
+			<Box
+				w={3}
+				h={3}
+				background={color.hexColor}
+				cursor="pointer"
+				onClick={() => setCurrentImage(color.image)}
+			></Box>
 		</Tooltip>
 	);
 };
--- src/customer/components/ProductCard.tsx
+++ src/customer/components/ProductCard.tsx
@@ -1,40 +1,50 @@
 import { Badge, Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Product } from "../../app/product/productApiSlice";
 import ColorBox from "./ColorBox";
 
 const ProductCard = ({ product }: { product: Product }) => {
+	const [currentImage, setCurrentImage] = useState(product.color[0].image);
+
 	return (
-		<Link to={`/products/${product.id}`}>
-			<Box
-				_hover={{ transform: "scale(1.01)" }}
-				transition={"transform 0.1s ease-out"}
-			>
-				<Image
-					src={product.color[0].image}
-					alt={product.name}
-					w="280px"
-					h="330px"
-					objectFit="cover"
-				/>
-			</Box>
-			<VStack mt={4} px={1} align="start">
-				<HStack justifyContent="space-between" w="100%">
+		<VStack align="start">
+			<Link to={`/products/${product.id}`}>
+				<Box
+					_hover={{ transform: "scale(1.01)" }}
+					transition={"transform 0.1s ease-out"}
+				>
+					<Image
+						src={currentImage}
+						alt={product.name}
+						w="280px"
+						h="330px"
+						objectFit="cover"
+					/>
+				</Box>
+				<HStack justifyContent="space-between" w="100%" mt={4} px={1}>
 					<Text fontWeight="bold" fontSize="small">
 						{product.name.toUpperCase()}
 					</Text>
 					<Badge colorScheme="green">{product.category.name}</Badge>
 				</HStack>
+			</Link>
+
+			<VStack px={1} align="start">
 				<Text fontWeight="semibold" fontSize="small">
 					Rs. {product.price}
 				</Text>
-				<HStack>
+				<HStack justifyContent="start">
 					{product.color.map((color) => (
-						<ColorBox key={color.hexColor} color={color} />
+						<ColorBox
+							key={color.hexColor}
+							color={color}
+							setCurrentImage={setCurrentImage}
+						/>
 					))}
 				</HStack>
 			</VStack>
-		</Link>
+		</VStack>
 	);
 };
 
