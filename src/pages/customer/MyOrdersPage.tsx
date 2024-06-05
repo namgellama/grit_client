b934b60e12bd0dade87b1ecb41cb4e53f90a0b16
Namgel <namgeltamang14@gmail.com>
2024-06-05 10:47:34 +0545

    Build MyOrdersPage

--- src/pages/customer/MyOrdersPage.tsx
+++ src/pages/customer/MyOrdersPage.tsx
@@ -1,5 +1,58 @@
+import {
+	TableContainer,
+	Table,
+	TableCaption,
+	Thead,
+	Tr,
+	Th,
+	Tbody,
+	Td,
+	Tfoot,
+} from "@chakra-ui/react";
+import { MyContainer } from "../../components";
+import { useAppSelector } from "../../app/hooks";
+import { useGetMyOrdersQuery } from "../../app/features/order/orderApiSlice";
+import { getStringDate } from "../../utilities/getStringDate";
+import { Link } from "react-router-dom";
+
 const MyOrdersPage = () => {
-	return <div>MyOrdersPage</div>;
+	const { user } = useAppSelector((state) => state.auth);
+
+	const { data: orders } = useGetMyOrdersQuery(user?.token || "");
+	return (
+		<MyContainer width="7xl">
+			<TableContainer>
+				<Table variant="simple">
+					<Thead>
+						<Tr>
+							<Th>Order Id</Th>
+							<Th isNumeric>Total Price</Th>
+							<Th>Status</Th>
+							<Th>Ordered Date</Th>
+							<Th>Payment Method</Th>
+							<Th>Payment Status</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{orders?.map((order) => (
+							<Tr key={order.id}>
+								<Td _hover={{ textDecoration: "underline" }}>
+									<Link to={`/orders/mine/${order.id}`}>
+										{order.id}
+									</Link>
+								</Td>
+								<Td isNumeric>{order.totalPrice}</Td>
+								<Td>{order.status}</Td>
+								<Td>{getStringDate(order.createdAt)}</Td>
+								<Td>{order.payment?.method}</Td>
+								<Td>{order.payment?.status}</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</TableContainer>
+		</MyContainer>
+	);
 };
 
 export default MyOrdersPage;
--- src/utilities/getStringDate.ts
+++ src/utilities/getStringDate.ts
@@ -1,4 +1,8 @@
 export const getStringDate = (data: Date) => {
+	return new Date(data).toDateString();
+};
+
+export const getStringDateTime = (data: Date) => {
 	return (
 		new Date(data).toDateString() +
 		", " +
