f06c3846d07c51bc9fd48df1d9289f7f7a929a3d
Namgel <namgeltamang14@gmail.com>
2024-06-07 10:26:58 +0545

    Build MyOrderDetailPage

--- src/pages/customer/MyOrderDetailPage.tsx
+++ src/pages/customer/MyOrderDetailPage.tsx
@@ -1,5 +1,129 @@
+import { useParams } from "react-router-dom";
+import { useGetMyOrderQuery } from "../../app/features/order/orderApiSlice";
+import { useAppSelector } from "../../app/hooks";
+import { MyContainer } from "../../components";
+import {
+	Heading,
+	HStack,
+	Text,
+	VStack,
+	Image,
+	Divider,
+	Flex,
+	Box,
+	Card,
+	CardBody,
+	Badge,
+} from "@chakra-ui/react";
+import { getStringDateTime } from "../../utilities/getStringDate";
+
 const MyOrderDetailPage = () => {
-	return <div>MyOrderDetailPage</div>;
+	const { id } = useParams();
+	const { user } = useAppSelector((state) => state.auth);
+
+	const { data: order } = useGetMyOrderQuery({
+		id: id ?? "",
+		token: user?.token ?? "",
+	});
+
+	return (
+		<MyContainer width="4xl">
+			<Card>
+				<CardBody>
+					<Flex direction="column" gap={4}>
+						<Heading fontSize="large">Order ID: {id}</Heading>
+						<Text>
+							Order Date:{" "}
+							{order && getStringDateTime(order?.createdAt)}
+						</Text>
+					</Flex>
+
+					<Divider borderColor="gray" my={8} />
+
+					<VStack align="start">
+						{order?.orderItems.map((orderItem) => (
+							<HStack key={orderItem.id} w="100%">
+								<Image
+									src={
+										orderItem.product.color.find(
+											(color) =>
+												color.colorName ===
+												orderItem.color
+										)?.image
+									}
+									alt={orderItem.product.name}
+									w="80px"
+									h="80px"
+								/>
+								<HStack justify="space-between" w="100%">
+									<VStack>
+										<Text>{orderItem.product.name}</Text>
+										<Text>
+											{orderItem.color} | {orderItem.size}
+										</Text>
+									</VStack>
+									<VStack justifySelf="end">
+										<Text fontWeight="bold">
+											Rs. {orderItem.unitTotalPrice}
+										</Text>
+										<Text fontSize="small">
+											Rs. {orderItem.unitTotalPrice} x{" "}
+											{orderItem.quantity}
+										</Text>
+									</VStack>
+								</HStack>
+							</HStack>
+						))}
+					</VStack>
+
+					<Divider borderColor="gray" my={8} />
+
+					<Box>
+						<Text fontWeight="bold" mb={2}>
+							Order Summary
+						</Text>
+						<HStack justify="space-between">
+							<Text>Subtotal</Text>
+							<Text fontWeight="bold">
+								Rs. {order?.totalPrice}
+							</Text>
+						</HStack>
+					</Box>
+
+					<Divider borderColor="gray" my={8} />
+
+					<HStack justify="space-between" align="start">
+						<Box>
+							<Text fontWeight="bold" mb={2}>
+								Payment
+							</Text>
+							<Text>
+								{order?.payment.method}
+								<Badge colorScheme="green" ml={4}>
+									{order?.payment.status}
+								</Badge>
+							</Text>
+						</Box>
+						<Box>
+							<Text fontWeight="bold" mb={2}>
+								Delivery
+							</Text>
+							<Text>
+								{order?.address?.addressLine1},{" "}
+								{order?.address?.addressLine2}
+							</Text>
+							<Text>
+								{order?.address?.city},{" "}
+								{order?.address?.country}
+								{order?.address?.postalCode &&
+									`- ${order?.address?.postalCode}`}
+							</Text>
+						</Box>
+					</HStack>
+				</CardBody>
+			</Card>
+		</MyContainer>
+	);
 };
 
 export default MyOrderDetailPage;
