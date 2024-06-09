8758ae589773535104e481be1c38dceca37292da
Namgel <namgeltamang14@gmail.com>
2024-06-09 10:36:05 +0545

    Update data type

--- src/utilities/data.ts
+++ src/utilities/data.ts
@@ -4,8 +4,24 @@ export const cities = [
 	{ name: "Bhaktapur", value: 130 },
 	{ name: "Others", value: 150 },
 ];
+
 export const countries = ["Nepal"];
+
 export const paymentMethods = [
 	{ name: "Cash", value: "CASH" },
 	{ name: "Esewa", value: "ESEWA" },
+];
+
+export const orderStatuses = [
+	{ name: "Order Pending", value: "PENDING" },
+	{ name: "Order Shipped", value: "SHIPPED" },
+	{ name: "Order Delivered", value: "DELIVERED" },
+	{ name: "Order Cancelled", value: "CANCELLED" },
+];
+
+export const paymentStatuses = [
+	{ name: "Payment Pending", value: "PENDING" },
+	{ name: "Payment Completed", value: "COMPLETED" },
+	{ name: "Payment Failed", value: "FAILED" },
+	{ name: "Payment Refunded", value: "REFUNDED" },
 ];
