472d456213bf4283a220781bf20c8987986ce63b
Namgel <namgeltamang14@gmail.com>
2024-06-10 10:49:13 +0545

    Fix navigation when admin logs in

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { logout } from "./app/features/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { AdminNavBar, NavBar } from "./components";
@@ -10,10 +10,15 @@ function App() {
 
 	const dispatch = useAppDispatch();
 	const navigate = useNavigate();
+	const location = useLocation();
 
 	useEffect(() => {
-		if (user?.role === "Admin") navigate("/dashboard");
-	}, [user, navigate]);
+		if (
+			user?.role === "Admin" &&
+			!location.pathname.startsWith("/dashboard")
+		)
+			navigate("/dashboard");
+	}, [user, navigate, location]);
 
 	const handleLogout = () => {
 		dispatch(logout());
