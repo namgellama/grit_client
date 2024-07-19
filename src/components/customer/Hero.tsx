9ef57cab97f7f8660c38391e21b1bacc5f9948f5
Namgel <namgeltamang14@gmail.com>
2024-07-19 21:40:32 +0545

    Set up hero component images

--- src/components/customer/Hero.tsx
+++ src/components/customer/Hero.tsx
@@ -1,16 +1,36 @@
-import { Box, Image } from "@chakra-ui/react";
-import heroImg from "../../assets/hero-img.png";
+import { Grid, Image, Box } from "@chakra-ui/react";
+import ReactPlayer from "react-player";
+import gritVideo from "../../assets/grit.mov";
+import heroImg1 from "../../assets/hero1.jpg";
+import heroImg2 from "../../assets/hero2.jpg";
 
 const Hero = () => {
 	return (
-		<Box width="100vw" height="85vh">
-			<Image
-				src={heroImg}
-				width="100%"
-				height="100%"
-				objectFit="cover"
-				alt="Hero Image"
-			/>
+		<Box minHeight="80vh" overflow="hidden">
+			<Grid templateColumns="repeat(3, 1fr)" height="80vh" gap={2}>
+				<Image
+					src={heroImg1}
+					width="100%"
+					height="100%"
+					objectFit="cover"
+					alt="Hero Image"
+				/>
+				<ReactPlayer
+					url={gritVideo}
+					playing
+					width="100%"
+					height="100%"
+					loop
+					muted
+				/>
+				<Image
+					src={heroImg2}
+					width="100%"
+					height="100%"
+					objectFit="cover"
+					alt="Hero Image"
+				/>
+			</Grid>
 		</Box>
 	);
 };
