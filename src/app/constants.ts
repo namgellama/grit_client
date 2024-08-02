471d314e3b1b06e3c8cd71e093e76829018bcf0b
Namgel <namgeltamang14@gmail.com>
2024-08-02 15:42:36 +0545

    Update BASE_URL

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,4 @@
-export const BASE_URL =
-	import.meta.env.VITE_APP_ENV === "development"
-		? "http://localhost:3000"
-		: import.meta.env.VITE_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const PRODUCT_URL = "/api/products";
 export const CATEGORY_URL = "/api/categories";
