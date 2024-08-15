74f3829a132534b6cb8705fd47cedb928d52b576
Namgel <namgeltamang14@gmail.com>
2024-08-15 13:21:56 +0545

    Update product cache

--- src/app/features/order/orderApiSlice.ts
+++ src/app/features/order/orderApiSlice.ts
@@ -78,7 +78,12 @@ export const orderApiSlice = apiSlice.injectEndpoints({
 					Authorization: `Bearer ${token}`,
 				},
 			}),
-			invalidatesTags: ["MyOrders"],
+			invalidatesTags: [
+				"MyOrders",
+				"Product",
+				"Products",
+				"AdminProduct",
+			],
 		}),
 
 		updateOrder: builder.mutation<
